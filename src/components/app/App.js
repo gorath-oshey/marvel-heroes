dc73f3e490b60a18e37d1a5d8b2528478db3bd42
Radyslav Dmytrenko <sheogoradik@gmail.com>
2023-04-11 14:18:43 +0300

    Added character search form

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,10 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
+import {MainPage, ComicsPage, Page404} from '../pages';
 import AppHeader from '../appHeader/AppHeader';
+import SinglePage from "../pages/SinglePage";
+import SingleCharacterLayout from "../pages/singleCharacterLayout/SingleCharacterLayout";
+import SingleComicLayout from "../pages/singleComicLayout/SingleComicLayout";
 
 const App = () => {
     return (
@@ -12,7 +15,8 @@ const App = () => {
                     <Routes>
                         <Route path="/" element={<MainPage/>}/>
                         <Route path="/comics" element={<ComicsPage/>}/>
-                        <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                        <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic'/>}/>
+                        <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>}/>
                         <Route path="*" element={<Page404/>}/>
                     </Routes>
                 </main>
